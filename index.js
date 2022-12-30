9670ac9fea2940b5af896f582071150818577ddc
vrnnncl <97320132+vrnnncl@users.noreply.github.com>
2022-12-30 21:13:50 +0300

    test expand()

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ const webapp = window.Telegram.WebApp;
 
 const documentBody = document.querySelector('body');
 
-documentBody.textContent = `Your Bot API verison: ${webapp.version}`;
+webapp.expand();
+
+documentBody.textContent = `Your Bot API version: ${webapp.version}`;
