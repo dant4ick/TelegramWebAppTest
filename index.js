89289a54fa5f99ae54ebd617513e56a5942e70fa
vrnnncl <97320132+vrnnncl@users.noreply.github.com>
2022-12-30 21:35:13 +0300

    test telegram webapp parameters

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ const webapp = window.Telegram.WebApp;
 
 const documentBody = document.querySelector('body');
 
-webapp.expand();
+function addLine(text, connectTo) {
+  const line = document.createElement('p');
+  line.textContent = text;
+  connectTo.appendChild(line);
+}
 
-documentBody.textContent = `Your Bot API version: ${webapp.version}`;
+container = document.createElement('div');
+addLine(`Your Bot API version: ${webapp.version}`, container);
+addLine(`Your platform: ${webapp.platform}`, container);
+addLine(`Your color scheme: ${webapp.colorScheme}`, container);
+addLine(`Web App is ${webapp.isExpanded ? '' : 'not'} expanded`, container);
+addLine(`Your viewport is ${webapp.viewportHeight}px`, container);
+addLine(`Your stable viewport is ${webapp.viewportStableHeight}px`, container);
+addLine(`Your header color is ${webapp.headerColor}`, container);
+addLine(`Your background color is ${webapp.backgroundColor}`, container);
+documentBody.appendChild(container);
